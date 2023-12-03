4623991375a40f0f9d3b9cdb02eabbfe6eafc64a
ykomarlu <30438247+ykomarlu@users.noreply.github.com>
2023-12-03 16:24:49 -0500

    12/2 Problem

---
+++ 12_2_Cube_Conundrum/Cube_Conundrum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Cube_Conundrum {
+    Scanner scan = new Scanner("input.txt");
+    // I need to go line by line and then delimit the String by semicolons
+    // I can identify which color easily by seeing in the first letter is either r,g or b
+    // I need to map the numbers stored by each number in a map
+    // I then can see if the individual totals are possible for each substring
+    //if so then add the id to a running sum
+    //if not then skip
+    //return sum
+
+    public 
+}
